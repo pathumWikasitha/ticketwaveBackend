2e5cf6cf5b0732f6ae9651fe20388f91357f5dba
Pathum Wikastha <pathumrox2020@gmal.com>
2024-11-16 22:49:45 +0530

    7

--- src/main/java/com/ticketWave/ticketWave/controller/CustomerController.java
+++ src/main/java/com/ticketWave/ticketWave/controller/CustomerController.java
@@ -1,11 +1,42 @@
 package com.ticketWave.ticketWave.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ticketWave.ticketWave.dto.CustomerDTO;
+import com.ticketWave.ticketWave.dto.UserDTO;
+import com.ticketWave.ticketWave.dto.VendorDTO;
+import com.ticketWave.ticketWave.model.Ticket;
+import com.ticketWave.ticketWave.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/customer")
 @CrossOrigin
 public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/{customerID}")
+    public UserDTO getCustomer(@PathVariable int customerID) {
+        UserDTO userDTO = customerService.getCustomer(customerID);
+        if (userDTO == null) {
+            return null;
+        }
+        return userDTO;
+    }
+
+    @PostMapping("/register")
+    public void registerCustomer(@RequestBody CustomerDTO customerDTO) {
+        customerService.registerCustomer(customerDTO);
+    }
+
+    @PutMapping("/update/{customerID}")
+    public void updateCustomer(@PathVariable int customerID, @RequestBody CustomerDTO customerDTO) {
+        customerService.updateCustomer(customerID,customerDTO);
+    }
+
+    @PostMapping("/{customerID}/purchaseTicket")
+    public void purchaseTicket(@PathVariable int customerID, @RequestBody Ticket ticket) {
+        customerService.purchaseTicket(customerService, ticket);
+    }
 }
--- src/main/java/com/ticketWave/ticketWave/service/CustomerService.java
+++ src/main/java/com/ticketWave/ticketWave/service/CustomerService.java
@@ -1,6 +1,11 @@
 package com.ticketWave.ticketWave.service;
 
-import com.ticketWave.ticketWave.repo.CustomerRepo;
+import com.ticketWave.ticketWave.dto.CustomerDTO;
+import com.ticketWave.ticketWave.dto.UserDTO;
+import com.ticketWave.ticketWave.model.Customer;
+import com.ticketWave.ticketWave.model.Ticket;
+import com.ticketWave.ticketWave.model.User;
+import com.ticketWave.ticketWave.repo.UserRepo;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +14,15 @@ import java.util.List;
 
 @Service
 public class CustomerService {
+    private final UserRepo userRepo;
     private CustomerRepo customerRepo;
     private ModelMapper modelMapper;
     private final List<Thread> customerThreads = new ArrayList<>();
 
-    public CustomerService(ModelMapper modelMapper, CustomerRepo customerRepo) {
+    public CustomerService(ModelMapper modelMapper, CustomerRepo customerRepo, UserRepo userRepo) {
         this.customerRepo = customerRepo;
         this.modelMapper = modelMapper;
+        this.userRepo = userRepo;
     }
 
     public void stopCustomerThreads() {
@@ -23,5 +30,27 @@ public class CustomerService {
             thread.interrupt();
         }
         customerThreads.clear();
+    }
+
+    public UserDTO getCustomer(int customerID) {
+        UserDTO userDTO = modelMapper.map(userRepo.findByUserID(customerID), UserDTO.class);
+        userDTO.setPassword("");
+        return userDTO;
+    }
+
+    public void registerCustomer(CustomerDTO customerDTO) {
+        Customer customer = modelMapper.map(customerDTO, Customer.class);
+        customerRepo.save(customer);
+    }
+
+    public void updateCustomer(int customerID, CustomerDTO customerDTO) {
+        User user = userRepo.findByUserID(customerID);
+        if (user != null) {
+            userRepo.save(modelMapper.map(customerDTO, Customer.class));
+        }
+        System.out.println("Customer "+customerID+" not found");
+    }
+
+    public void purchaseTicket(CustomerService customerService, Ticket ticket) {
     }
 }
